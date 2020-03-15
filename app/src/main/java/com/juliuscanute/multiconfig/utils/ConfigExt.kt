222044f17342cb791dd50440cca58295b6a110fe
TheJuliuscanute <juliuscanute@touchcapture.net>
2020-03-15 15:13:36 +1100

    rename dependency initializer

--- app/src/main/java/com/juliuscanute/multiconfig/utils/ConfigExt.kt
+++ app/src/main/java/com/juliuscanute/multiconfig/utils/ConfigExt.kt
@@ -4,7 +4,7 @@ import android.content.Intent
 import androidx.activity.ComponentActivity
 import androidx.lifecycle.ViewModel
 import com.juliuscanute.multiconfig.MultiConfig
-import com.juliuscanute.multiconfig.di.DependencyHandler
+import com.juliuscanute.multiconfig.di.StartMultiConfig
 import model.ConfigurationManager
 import kotlin.reflect.KProperty
 
@@ -12,15 +12,15 @@ import kotlin.reflect.KProperty
 class ConfigManagerInitializer {
 
     operator fun getValue(service: ComponentActivity, property: KProperty<*>): ConfigurationManager {
-        return DependencyHandler.getConfigurationManager()
+        return StartMultiConfig.getConfigurationManager()
     }
 
     operator fun getValue(service: ViewModel, property: KProperty<*>): ConfigurationManager {
-        return DependencyHandler.getConfigurationManager()
+        return StartMultiConfig.getConfigurationManager()
     }
 
     operator fun getValue(service: MultiConfig, property: KProperty<*>): ConfigurationManager {
-        return DependencyHandler.getConfigurationManager()
+        return StartMultiConfig.getConfigurationManager()
     }
 
 }
@@ -28,6 +28,6 @@ class ConfigManagerInitializer {
 
 class IntentInitializer {
     operator fun getValue(service: ComponentActivity, property: KProperty<*>): Intent {
-        return DependencyHandler.getIntent()
+        return StartMultiConfig.getIntent()
     }
 }
