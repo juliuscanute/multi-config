0768d82a65225392f7dcca1df405d52ce27eff0b
TheJuliuscanute <juliuscanute@touchcapture.net>
2020-03-23 21:37:52 +1100

    Apply swift DSL

--- iosApp/iosApp/AppDelegate.swift
+++ iosApp/iosApp/AppDelegate.swift
@@ -5,10 +5,15 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+        window = UIWindow(frame: UIScreen.main.bounds)
+        window?.rootViewController = UIStoryboard(name: "Main", bundle: nil).instantiateInitialViewController()
+        window?.makeKeyAndVisible()
         return true
     }
 
-    func applicationWillResignActive(_ application: UIApplication) {}
+    func applicationWillResignActive(_ application: UIApplication) {
+        
+    }
 
     func applicationDidEnterBackground(_ application: UIApplication) {}
 
---
+++ iosApp/iosApp/Environment.swift
@@ -0,0 +1,65 @@
+//
+//  Environment.swift
+//  iosApp
+//
+//  Created by Julius Canute on 22/3/20.
+//
+
+import Foundation
+import app
+
+public func appConfig(apply closure: (AppConfigurationBuilder) -> Void) -> NSMutableArray {
+    let builder = AppConfigurationBuilder()
+    closure(builder)
+    return builder.build()
+}
+
+func setup() -> NSMutableArray {
+    return appConfig {
+        $0.config(environment: "DEV") {
+            $0.switch {
+                $0.key = "A"
+                $0.description = "A-D"
+                $0.switchValue = false
+            }
+        }
+        
+        $0.config(environment:"SIT") {
+            $0.range {
+                $0.key = "B"
+                $0.description = "B-D"
+                $0.min = 1
+                $0.max = 100
+                $0.step = 2
+                $0.currentValue = 50
+            }
+        }
+        
+        $0.config(environment:"UAT") {
+            $0.editable {
+                $0.key = "C"
+                $0.description = "C-D"
+                $0.currentValue = "C-V"
+            }
+        }
+        
+        $0.config(environment:"PROD") {
+            $0.choice {
+                $0.key = "D"
+                $0.description = "D-D"
+                $0.currentChoiceIndex = 0
+                $0.item {
+                    $0.description = "E"
+                }
+                $0.item {
+                    $0.description = "F"
+                }
+                $0.item {
+                    $0.description = "G"
+                }
+            }
+        }
+    }
+}
+
+
