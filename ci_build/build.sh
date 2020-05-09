448ec58b65aec01642420237a737c4dedf9eb930
TheJuliuscanute <juliuscanute@touchcapture.net>
2020-05-09 17:19:02 +1000

    shorten logs

--- ci_build/build.sh
+++ ci_build/build.sh
@@ -1,8 +1,29 @@
 #!/bin/bash
+shorten () {
+	local helpstring="Truncate each line of the input to X characters\n\t-l              Shorten from left side\n\t-s STRING         replace truncated characters with STRING\n\n\t$ ls | shorten -s ... 15"
+	local ellip="" left=false
+	OPTIND=1
+	while getopts "hls:" opt; do
+		case $opt in
+			l) left=true ;;
+			s) ellip=$OPTARG ;;
+			h) echo -e $helpstring; return;;
+			*) return 1;;
+		esac
+	done
+	shift $((OPTIND-1))
+
+	if $left; then
+		cat | sed -E "s/.*(.{${1-70}})$/${ellip}\1/"
+	else
+		cat | sed -E "s/(.{${1-70}}).*$/\1${ellip}/"
+	fi
+}
+
 buildFramework(){
   IOS_SDK=$1
   CONFIGURATION=$2
-  xcodebuild -sdk "${IOS_SDK}" -configuration "${CONFIGURATION}" -workspace multiconfig-ios/multiconfig-ios.xcworkspace -scheme multiconfig-ios clean build CODE_SIGNING_ALLOWED=NO
+  xcodebuild -sdk "${IOS_SDK}" -configuration "${CONFIGURATION}" -workspace multiconfig-ios/multiconfig-ios.xcworkspace -scheme multiconfig-ios clean build CODE_SIGNING_ALLOWED=NO | shorten -s ... 80
   export BUILD_DIR=$(xcodebuild -scheme multiconfig-ios -workspace multiconfig-ios/multiconfig-ios.xcworkspace ONLY_ACTIVE_ARCH=NO -sdk "${IOS_SDK}" -configuration "${CONFIGURATION}" -showBuildSettings | grep -m 1 "BUILT_PRODUCTS_DIR" | grep -oEi "\/.*")
 }
 
